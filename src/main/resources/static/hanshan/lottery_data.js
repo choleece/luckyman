874cc37a6b7301ed43f1cafd065cd536e1d4cd7b
choleece <chaolizheng@sf-express.com>
2018-10-26 11:56:15 +0800

    红包抽奖

--- src/main/resources/static/hanshan/lottery_data.js
+++ src/main/resources/static/hanshan/lottery_data.js
@@ -54,214 +54,224 @@ var nextFrame = window.requestAnimationFrame       ||
     // 初始移动是的定时动画变量
     timer = null,
     // 全局 setTimeout 定时任务指定变量
-    setout_time = null;
+    setout_time = null,
+    // 是否显示红包modal
+    show_modal = false;
 
-function getData() {
-
-    var data = [
-        {
-            "nameen": "avatar1",
-            "namezh": "蒋勇",
-            "wish": "新年快乐，们委的交型关确音几心她民知历解。"
-        },
-        {
-            "nameen": "avatar2",
-            "namezh": "于桂英",
-            "wish": "新年快乐，龙公平儿叫许车际展去状第论。"
-        },
-        {
-            "nameen": "avatar3",
-            "namezh": "徐超",
-            "wish": "新年快乐，十小叫社法革领事因水务住酸事义小。"
-        },
-        {
-            "nameen": "avatar4",
-            "namezh": "孙秀兰",
-            "wish": "新年快乐，得表新传目物九切设清志在油几口名调一再。"
-        },
-        {
-            "nameen": "avatar5",
-            "namezh": "张娟",
-            "wish": "新年快乐，片土列们准军看上调划准常员原进。"
-        },
-        {
-            "nameen": "avatar6",
-            "namezh": "潘秀兰",
-            "wish": "新年快乐，九论期要着音区内公观积。"
-        },
-        {
-            "nameen": "avatar7",
-            "namezh": "任伟",
-            "wish": "新年快乐，了教公没基改特低体细角好无二联片里都系。"
-        },
-        {
-            "nameen": "avatar8",
-            "namezh": "何刚",
-            "wish": "新年快乐，理由正用点美最属持象低集性期部条。"
-        },
-        {
-            "nameen": "avatar9",
-            "namezh": "万艳",
-            "wish": "新年快乐，几法完般石度成资位今派至林转面则改。"
-        },
-        {
-            "nameen": "avatar10",
-            "namezh": "田秀英",
-            "wish": "新年快乐，西使命带值直分把史达信且表计管海。"
-        },
-        {
-            "nameen": "avatar11",
-            "namezh": "崔伟",
-            "wish": "新年快乐，两从元处经争复当场导规起规织之组。"
-        },
-        {
-            "nameen": "avatar12",
-            "namezh": "朱秀兰",
-            "wish": "新年快乐，况机比来十回是算军传方照先想。"
-        },
-        {
-            "nameen": "avatar13",
-            "namezh": "金静",
-            "wish": "新年快乐，土及例育此求运大立价标点。"
-        },
-        {
-            "nameen": "avatar14",
-            "namezh": "韩伟",
-            "wish": "新年快乐，外青号由参她于空习天品建平运同就。"
-        },
-        {
-            "nameen": "avatar15",
-            "namezh": "邹勇",
-            "wish": "新年快乐，划完面克用规五看八下地什群金下许公条话。"
-        },
-        {
-            "nameen": "avatar16",
-            "namezh": "许强",
-            "wish": "新年快乐，前走式调细确林资易山把。"
-        },
-        {
-            "nameen": "avatar17",
-            "namezh": "赵杰",
-            "wish": "新年快乐，线很持装布二教想新马九土际料眼分第精。"
-        },
-        {
-            "nameen": "avatar18",
-            "namezh": "孟明",
-            "wish": "新年快乐，本件什有量今据收导我那无文。"
-        },
-        {
-            "nameen": "avatar19",
-            "namezh": "杨平",
-            "wish": "新年快乐，收般得可史常办现他把业千严么史广当说用。"
-        },
-        {
-            "nameen": "avatar20",
-            "namezh": "史敏",
-            "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。"
-        }
-    ];
-
-    var award_1 = localStorage.getItem('award_1');
-    var award_2 = localStorage.getItem('award_2');
-    var award_3 = localStorage.getItem('award_3');
-    var award_4 = localStorage.getItem('award_4');
-
-    var awards = [];
-    if (award_1) {
-        award_1 = JSON.parse(award_1);
-        awards = awards.concat(award_1);
-    }
-    if (award_2) {
-        award_2 = JSON.parse(award_2);
-        awards = awards.concat(award_2);
-    }
-    if (award_3) {
-        award_3 = JSON.parse(award_3);
-        awards = awards.concat(award_3);
-    }
-    if (award_4) {
-        award_4 = JSON.parse(award_4);
-        awards = awards.concat(award_4);
-    }
-
-    var arr = [];
-    for (var i = 0; i < data.length; i++) {
-        var flag = false;
-        var item = data[i];
-        for (var j = 0; j < awards.length; j++) {
-            if (item.namezh === awards[j].namezh && item.nameen === awards[j].nameen) {
-                flag = true;
-                break;
-            }
-        }
-        // 去掉已经中将的人员
-        if (!flag) {
-            arr.push(item);
-        }
-    }
-    lottery_datas = arr;
-    $('#lottery-wrap').html( _.template($('#lotterycon-tpl').html(), lottery_datas))
-}
+// function getData() {
+//
+//     var data = [
+//         {
+//             "nameen": "avatar1",
+//             "namezh": "蒋勇",
+//             "wish": "新年快乐，们委的交型关确音几心她民知历解。"
+//         },
+//         {
+//             "nameen": "avatar2",
+//             "namezh": "于桂英",
+//             "wish": "新年快乐，龙公平儿叫许车际展去状第论。"
+//         },
+//         {
+//             "nameen": "avatar3",
+//             "namezh": "徐超",
+//             "wish": "新年快乐，十小叫社法革领事因水务住酸事义小。"
+//         },
+//         {
+//             "nameen": "avatar4",
+//             "namezh": "孙秀兰",
+//             "wish": "新年快乐，得表新传目物九切设清志在油几口名调一再。"
+//         },
+//         {
+//             "nameen": "avatar5",
+//             "namezh": "张娟",
+//             "wish": "新年快乐，片土列们准军看上调划准常员原进。"
+//         },
+//         {
+//             "nameen": "avatar6",
+//             "namezh": "潘秀兰",
+//             "wish": "新年快乐，九论期要着音区内公观积。"
+//         },
+//         {
+//             "nameen": "avatar7",
+//             "namezh": "任伟",
+//             "wish": "新年快乐，了教公没基改特低体细角好无二联片里都系。"
+//         },
+//         {
+//             "nameen": "avatar8",
+//             "namezh": "何刚",
+//             "wish": "新年快乐，理由正用点美最属持象低集性期部条。"
+//         },
+//         {
+//             "nameen": "avatar9",
+//             "namezh": "万艳",
+//             "wish": "新年快乐，几法完般石度成资位今派至林转面则改。"
+//         },
+//         {
+//             "nameen": "avatar10",
+//             "namezh": "田秀英",
+//             "wish": "新年快乐，西使命带值直分把史达信且表计管海。"
+//         },
+//         {
+//             "nameen": "avatar11",
+//             "namezh": "崔伟",
+//             "wish": "新年快乐，两从元处经争复当场导规起规织之组。"
+//         },
+//         {
+//             "nameen": "avatar12",
+//             "namezh": "朱秀兰",
+//             "wish": "新年快乐，况机比来十回是算军传方照先想。"
+//         },
+//         {
+//             "nameen": "avatar13",
+//             "namezh": "金静",
+//             "wish": "新年快乐，土及例育此求运大立价标点。"
+//         },
+//         {
+//             "nameen": "avatar14",
+//             "namezh": "韩伟",
+//             "wish": "新年快乐，外青号由参她于空习天品建平运同就。"
+//         },
+//         {
+//             "nameen": "avatar15",
+//             "namezh": "邹勇",
+//             "wish": "新年快乐，划完面克用规五看八下地什群金下许公条话。"
+//         },
+//         {
+//             "nameen": "avatar16",
+//             "namezh": "许强",
+//             "wish": "新年快乐，前走式调细确林资易山把。"
+//         },
+//         {
+//             "nameen": "avatar17",
+//             "namezh": "赵杰",
+//             "wish": "新年快乐，线很持装布二教想新马九土际料眼分第精。"
+//         },
+//         {
+//             "nameen": "avatar18",
+//             "namezh": "孟明",
+//             "wish": "新年快乐，本件什有量今据收导我那无文。"
+//         },
+//         {
+//             "nameen": "avatar19",
+//             "namezh": "杨平",
+//             "wish": "新年快乐，收般得可史常办现他把业千严么史广当说用。"
+//         },
+//         {
+//             "nameen": "avatar20",
+//             "namezh": "史敏",
+//             "wish": "新年快乐，步根长无定体色厂集内白阶阶并以节口每养。"
+//         }
+//     ];
+//
+//     $("#total-signer").html("签到人数：" + data.length + "人");
+//
+//     if (data.length !== last_data.length) {
+//
+//         last_data = data;
+//
+//         var award_1 = localStorage.getItem('award_1');
+//         var award_2 = localStorage.getItem('award_2');
+//         var award_3 = localStorage.getItem('award_3');
+//         var award_4 = localStorage.getItem('award_4');
+//
+//         var awards = [];
+//         if (award_1) {
+//             award_1 = JSON.parse(award_1);
+//             awards = awards.concat(award_1);
+//         }
+//         if (award_2) {
+//             award_2 = JSON.parse(award_2);
+//             awards = awards.concat(award_2);
+//         }
+//         if (award_3) {
+//             award_3 = JSON.parse(award_3);
+//             awards = awards.concat(award_3);
+//         }
+//         if (award_4) {
+//             award_4 = JSON.parse(award_4);
+//             awards = awards.concat(award_4);
+//         }
+//
+//         var arr = [];
+//         for (var i = 0; i < data.length; i++) {
+//             var flag = false;
+//             var item = data[i];
+//             for (var j = 0; j < awards.length; j++) {
+//                 if (item.namezh === awards[j].namezh && item.nameen === awards[j].nameen) {
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             // 去掉已经中将的人员
+//             if (!flag) {
+//                 arr.push(item);
+//             }
+//         }
+//         lottery_datas = arr;
+//         $('#lottery-wrap').html( _.template($('#lotterycon-tpl').html(), lottery_datas))
+//     }
+// }
 
 /**
  * 获取数据
  */
-// function getData() {
-//     $.ajax({
-//         type: 'GET',
-//         url: 'signer',
-//         data: {},
-//         dataType: 'json',
-//         success: function (res) {
-//             if (res.code === 0) {
-//                 var data = res.data;
-//                 if (last_data.length !== data.length) {
-//                     last_data = data;
-//                     var award_1 = localStorage.getItem('award_1');
-//                     var award_2 = localStorage.getItem('award_2');
-//                     var award_3 = localStorage.getItem('award_3');
-//                     var award_4 = localStorage.getItem('award_4');
-//
-//                     var awards = [];
-//                     if (award_1) {
-//                         award_1 = JSON.parse(award_1);
-//                         awards = awards.concat(award_1);
-//                     }
-//                     if (award_2) {
-//                         award_2 = JSON.parse(award_2);
-//                         awards = awards.concat(award_2);
-//                     }
-//                     if (award_3) {
-//                         award_3 = JSON.parse(award_3);
-//                         awards = awards.concat(award_3);
-//                     }
-//                     if (award_4) {
-//                         award_4 = JSON.parse(award_4);
-//                         awards = awards.concat(award_4);
-//                     }
-//
-//                     var arr = [];
-//                     for (var i = 0; i < data.length; i++) {
-//                         var flag = false;
-//                         var item = data[i];
-//                         for (var j = 0; j < awards.length; j++) {
-//                             if (item.namezh === awards[j].namezh && item.nameen === awards[j].nameen) {
-//                                 flag = true;
-//                                 break;
-//                             }
-//                         }
-//                         // 去掉已经中将的人员
-//                         if (!flag) {
-//                             arr.push(item);
-//                         }
-//                     }
-//                     lottery_datas = arr;
-//                     $('#lottery-wrap').html( _.template($('#lotterycon-tpl').html(), lottery_datas));
-//                 }
-//             }
-//         }
-//     })
-// };
+function getData() {
+    $.ajax({
+        type: 'GET',
+        url: 'signer',
+        data: {},
+        dataType: 'json',
+        success: function (res) {
+            if (res.code === 0) {
+                var data = res.data;
+                $("#total-signer").html("签到人数：" + data.length + "人");
+                if (last_data.length !== data.length) {
+                    last_data = data;
+                    var award_1 = localStorage.getItem('award_1');
+                    var award_2 = localStorage.getItem('award_2');
+                    var award_3 = localStorage.getItem('award_3');
+                    var award_4 = localStorage.getItem('award_4');
+
+                    var awards = [];
+                    if (award_1) {
+                        award_1 = JSON.parse(award_1);
+                        awards = awards.concat(award_1);
+                    }
+                    if (award_2) {
+                        award_2 = JSON.parse(award_2);
+                        awards = awards.concat(award_2);
+                    }
+                    if (award_3) {
+                        award_3 = JSON.parse(award_3);
+                        awards = awards.concat(award_3);
+                    }
+                    if (award_4) {
+                        award_4 = JSON.parse(award_4);
+                        awards = awards.concat(award_4);
+                    }
+
+                    var arr = [];
+                    for (var i = 0; i < data.length; i++) {
+                        var flag = false;
+                        var item = data[i];
+                        for (var j = 0; j < awards.length; j++) {
+                            if (item.namezh === awards[j].namezh && item.nameen === awards[j].nameen) {
+                                flag = true;
+                                break;
+                            }
+                        }
+                        // 去掉已经中将的人员
+                        if (!flag) {
+                            arr.push(item);
+                        }
+                    }
+                    lottery_datas = arr;
+                    $('#lottery-wrap').html( _.template($('#lotterycon-tpl').html(), lottery_datas));
+                }
+            }
+        }
+    })
+};
 
 function initData() {
 
@@ -337,12 +347,13 @@ $(function(){
     initData();
 
     getData();
-    // setInterval(function(){
-    //     console.log(isStart);
-    //     if (!isStart) {
-    //         getData();
-    //     }
-    // }, 3000);
+    setInterval(function() {
+        console.log(isStart);
+        if (!isStart && !show_modal) {
+            console.log('获取数据');
+            getData();
+        }
+    }, 3000);
 
     justGo(isMove);
 
@@ -353,6 +364,7 @@ $(function(){
             console.error('还没结束，无法重开！');
             return false;
         }
+        show_modal = false;
         $('#lottery-result').modal('hide');
     });
 
@@ -667,6 +679,7 @@ function stopLottery() {
     isStart = false;
     isMove = false;
     speed = 6;
+    show_modal = true;
 
     /*-------- 手动停止的方案 --------*/
     // 得到当前所在高度值
@@ -709,15 +722,16 @@ function stopLottery() {
                 cancelFrame(time02);
                 // 处理中奖后的相关样式效果
                 $('#lottery-wrap .lottery-list').eq(sure_index).addClass('sure-active');
-                var award_tpl = $('#awardcon-tpl').html();
-                var award_dom = substitute(award_tpl, award_tmp);
-                $('#award-0'+award).show();
-                if (award == 4) {
-                    $('#award-123').hide();
-                    $('#award-04').show();
-                    $('#award04-toggle').css('display', 'inline-block');
-                }
-                $('#award-0'+award+' .win').append(award_dom);
+                // var award_tpl = $('#awardcon-tpl').html();
+                console.log('这里是干什么的哟');
+                // var award_dom = substitute(award_tpl, award_tmp);
+                // $('#award-0'+award).show();
+                // if (award == 4) {
+                //     $('#award-123').hide();
+                //     $('#award-04').show();
+                //     $('#award04-toggle').css('display', 'inline-block');
+                // }
+                // $('#award-0'+award+' .win').append(award_dom);
             }
         });
     };
@@ -727,18 +741,25 @@ function stopLottery() {
 
     // 获取当前是拿一类奖项，1代表特等奖，2，3，4分别代表一、二、三等奖
     var award = $('#lottery-btn').data('award');
-    var sure_index_arr = [sure_index];
+    var lottery__total = lottery_datas.length;
+    var random_idx = Math.floor(Math.random()*lottery__total);
+    if (random_idx >= lottery__total) {
+        random_idx = lottery__total - 1;
+    }
+    var sure_index_arr = [random_idx];
     // 如果不是特等奖，其他的三类奖项均为一次开奖5人, 则上下取其二
     if (award !== 1) {
-        sure_index_arr = [sure_index - 2, sure_index - 1, sure_index, sure_index + 1, sure_index + 2];
+        sure_index_arr = [random_idx, (random_idx + 1) % lottery__total, (random_idx + 2) % lottery__total, (random_idx + 3) % lottery__total, (random_idx + 4) % lottery__total];
     }
 
     // 中奖人数组
     var lottery_arr = [];
     for (var i = 0; i < sure_index_arr.length; i++) {
         var index = sure_index_arr[i];
-        var lottery_name_zh = $('#lottery-wrap .lottery-list').eq(index).data('namezh');
-        var lottery_name_en = $('#lottery-wrap .lottery-list').eq(index).data('nameen');
+        // var lottery_name_zh = $('#lottery-wrap .lottery-list').eq(index).data('namezh');
+        // var lottery_name_en = $('#lottery-wrap .lottery-list').eq(index).data('nameen');
+        var lottery_name_zh = lottery_datas[index].namezh;
+        var lottery_name_en = lottery_datas[index].nameen;
         lottery_arr.push({'namezh': lottery_name_zh, 'nameen': lottery_name_en});
     }
 
@@ -800,6 +821,7 @@ function stopLottery() {
         drawAward(award, lottery_arr);
 
         can_stop = true;
+        show_modal = true;
         clearTimeout(arguments.callee);
 
         // 清除当前的定时任务
@@ -813,6 +835,7 @@ function stopLottery() {
 
 // canvas 绘制中奖结果
 function drawAward(award, arr, pic_format) {
+    show_modal = true;
     var canvas = document.getElementById('lottery-canvas');
     var context = canvas.getContext('2d');
     if (!pic_format) {
@@ -833,10 +856,24 @@ function drawAward(award, arr, pic_format) {
         context.fillStyle = '#D9AD61';
         context.font = "bold 6rem STKaiti";
 
-        for (var i = 0; i < arr.length; i++) {
-            var item = arr[i];
-            var height = item.namezh.length >= 3 ? 300 : 280;
-            context.fillText(item.namezh, height, 930 + 90 * i);
+        if (arr.length === 1) {
+            context.fillText(arr[0].namezh, 280, 1100);
+        } else {
+            var second = '', third = '';
+            for (var i = 0; i < arr.length; i++) {
+                var item = arr[i];
+                console.log('i: ' + i);
+                if (i === 0) {
+                    context.fillText(item.namezh, 280, 950);
+                }
+                if (i === 1 || i === 2) {
+                    second += item.namezh + ",";
+                } else {
+                    third += item.namezh + ",";
+                }
+            }
+            context.fillText(second.substr(0, second.length - 1), 150, 1050);
+            context.fillText(third.substr(0, third.length - 1), 150, 1150);
         }
     };
 }
